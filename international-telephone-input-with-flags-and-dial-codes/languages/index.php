d9969e39e2bc3ace43b45237cc3a9954cff816a5
Sean Murphy <sean@iamseanmurphy.com>
2025-05-18 19:16:45 +0000

    2.0.1

--- international-telephone-input-with-flags-and-dial-codes/languages/index.php
+++ international-telephone-input-with-flags-and-dial-codes/languages/index.php
@@ -4,7 +4,7 @@
  *
  * @since         2.0.0
  * @package       International_Telephone_Input_With_Flags_And_Dial_Codes
- * @subpackage    International_Telephone_Input_With_Flags_And_Dial_Codes/public
+ * @subpackage    International_Telephone_Input_With_Flags_And_Dial_Codes/languages
  * @author        Sajjad Hossain Sagor <sagorh672@gmail.com>
  */
 
